ae205df93e84f344b317a66f8e92ee8935ca8a05
tq <2310307125@qq.com>
2022-05-31 08:38:56 +0800

    ff

--- src/env.groovy
+++ src/env.groovy
@@ -17,6 +17,7 @@ pipeline{
         steps{
           echo "${CC} ${DEBUG_FLAGS}"
           echo "${__artifact_name}"
+          echo "${env.g_name}"
           // sh "printenv"
         }
       }
